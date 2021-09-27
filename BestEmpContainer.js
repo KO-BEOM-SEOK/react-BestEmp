aa5d5faac87a0fcfe053fde43102e2a83d5b10c3
KOBEOMSEOK <rhrhrhqjatjr@naver.com>
2021-09-27 19:05:51 +0900

    commit

--- BestEmpContainer.js
+++ BestEmpContainer.js
@@ -1,29 +1,56 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import HrAppBar from 'ERP/HR/util/HrAppBar'
-import BestEmp from './BestEmp';
 import Axios from 'axios';
 
 // ********** 2021/09/27 고범석   이달의 BEST 사원 **********
 const BestEmpContainer = () => {
 
     const box = useRef(null);
+    const [bestEmp, setBestEmp] = useState([]);
+
+    console.log('!! here !!');
+    console.log(bestEmp);
+    console.log(bestEmp.img);
 
     const clickFunc = () => {
         return (
             Axios.get(
                 "http://localhost:8282/hr/emp/bestEmp.do"
+            ).then(
+                ({ data }) => {
+                    console.log(data);
+                    console.log(data.bestEmp[0].empName);
+                    console.log(data.bestEmp[0].img);
+                    setBestEmp({
+                        empName: data.bestEmp[0].empName,
+                        deptCode: data.bestEmp[0].deptCode,
+                        deptName: data.bestEmp[0].deptName,
+                        img: data.bestEmp[0].img,
+                        weekdayWorkDays: data.bestEmp[0].weekdayWorkDays
+                    })
+
+                    box.current.src = require(`./img/${data.bestEmp[0].img}`);
+
+                }
             )
-            // box.current.src = require('./img/emp.png')
         )
     }
 
     return (
         <>
             <div>
-                <HrAppBar title='이달의 BEST 사원' />
+                <HrAppBar title='저번달의 BEST 사원' />
                 <br />
                 <br />
-                <h3 align='center'> 과연 이달의 BEST 사원은 누구일까요??</h3>
+                <h3 align='center'>
+                    {
+                        bestEmp.empName
+                            ? `!! 총 일수${bestEmp.weekdayWorkDays}일 로 ${bestEmp.deptName} 부서의 ${bestEmp.empName} 사원이 BEST 사원입니다 !!`
+                            : '과연 저번달의 BEST 사원은 누구일까요??'
+                    }
+                </h3>
+                <br />
+                <br />
                 <br />
                 <br />
                 <div style={
@@ -35,9 +62,20 @@ const BestEmpContainer = () => {
                 }>
                     <img src={require('./img/bestEmp.png')} onClick={clickFunc} ref={box} />
                 </div>
+                <br />
+                <br />
+
+                <h3 align='center'>
+                    {
+                        bestEmp.empName
+                            ? ` ${bestEmp.empName} 사원님 축하드립니다 이번달도 기대할게요~`
+                            : '궁금하시면 물음표를 클릭해 주세요~'
+                    }
+                </h3>
             </div>
         </>
     )
 }
 
 export default BestEmpContainer
+// ********** 2021/09/27 고범석   이달의 BEST 사원 ************
